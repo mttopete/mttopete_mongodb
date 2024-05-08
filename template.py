976052e9ce59a1934ea9677f531a63614a374227
mttopete <mateotopete@gmail.com>
2024-05-08 11:29:01 -0700

    updated file structure

---
+++ template.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+
+package_name = "mongodboperations"
+
+files = [
+    ".github/workflows/ci.yaml",
+    "src/__init__.py",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/mongo_crud.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/unit/unit.py",
+    "tests/integration/__init__.py",
+    "tests/integration/integration.py",
+    "init__setup__.py",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini"
+]
+
+for filepath in files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        #logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+            pass
